953fb8a960c592a359e9d30cb4e495ca05d324ef
Alexandre Sioufi <sioufia78@gmail.com>
2018-01-23 16:36:45 +0100

    Did exercice 1

--- test_folder/all_assigned.py
+++ test_folder/all_assigned.py
@@ -1,5 +1,23 @@
 #Criteria n° 1
-from .control_graph import main
+import sys
+sys.path.insert(0,sys.path[0][:len(sys.path[0])-12])
+from control_graph import apply_path
 
-L = set([2,3,5,6]) #List of the nodes made by assi
+TA = set([2,3,5,6]) #List of the labels with assignements
+
+X2 = {'X':-1} #For labels 2 and 6
+X3 = {'X':1} #For labels 3 and 6
+X5 = {'X':-1} #For label 5
+
+visited = set([])
+visited |= set(apply_path(X2))
+visited |= set(apply_path(X3))
+visited |= set(apply_path(X5))
+
+if len(TA.difference(visited)) == 0:
+    print("Test passed!")
+else:
+    print("Test not passed!")
+
+
 
