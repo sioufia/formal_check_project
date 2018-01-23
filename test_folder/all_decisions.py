4e9c7df709b01cfedaedab087904011b217ddeda
Seris <mathieu.seris@student.ecp.fr>
2018-01-23 16:34:44 +0100

    all decisions

--- test_folder/all_decisions.py
+++ test_folder/all_decisions.py
@@ -1,1 +1,22 @@
-from control_graph import *
+import sys
+sys.path.insert(0,sys.path[0][:len(sys.path[0])-12])
+
+from control_graph import apply_path
+
+L = {(2, 4), (3, 4), (5, "exit"), (6, "exit")}
+
+T = [
+    {"X": -1},
+    {"X": 2}
+]
+
+visited = set()
+for t in T:
+    path_visited = apply_path(t)
+    edge_visited = set((path_visited[i], path_visited[i + 1]) for i in range(len(path_visited) - 1))
+    visited |= edge_visited
+
+if L - visited:
+    print('test failed')
+else:
+    print('test passed')
