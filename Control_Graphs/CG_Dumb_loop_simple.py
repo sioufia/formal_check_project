5d8ffe3d4e5d552c200fa78afcead6c403f2b5c5
Alexandre Sioufi <sioufia78@gmail.com>
2018-01-29 11:11:42 +0100

    Corrected simple loop graph

--- Control_Graphs/CG_Dumb_loop_simple.py
+++ Control_Graphs/CG_Dumb_loop_simple.py
@@ -18,13 +18,13 @@ def CG_dumb_loop_simple():
     B2_1 = Boolean_Exp(lambda vars:vars['X']==1,"X=1")
     V2.add_next_edge(Edge(B2_1, C_skip, 4))
     B2_2 = Boolean_Exp(lambda vars:vars['X']!=1,"X!=1")
-    V2.add_next_edge(Edge(B2_1, C_skip, 2))
+    V2.add_next_edge(Edge(B2_2, C_skip, 3))
 
     V3 = Vertice(3)
-    def func2(variables):
+    def func3(variables):
         variables['X'] = variables['X'] - 1
         return variables
-    C3 = Command_Exp(func2 , "X:=1", type="assign")
+    C3 = Command_Exp(func3 , "X:=X-1", type="assign")
     V3.add_next_edge(Edge(B_True, C3, 2))
 
     V4 = Vertice(4)
@@ -36,11 +36,12 @@ def CG_dumb_loop_simple():
 
     Vexit = Vertice("exit")
     Control_Graph = Graph([V1,V2,V3,Vexit])
-    Control_Graph.add_simple_partial_paths((V1.label,V4.label),[{1,2,4},{1,2,3,2,4}])
+    Control_Graph.add_simple_partial_paths((V1.label,V4.label),[[1,2,4,"exit"],[1,2,3,2,4,"exit"]])
+    Control_Graph.add_simple_partial_paths((V3.label,V2.label),[[1,2,3,2,4,"exit"],[1,2,3,2,3,2,4,"exit"]])
     return Control_Graph
 
     if __name__ == "__main__":
-        CG = CG_dump_loop_simple()
+        CG = CG_dumb_loop_simple()
         print(CG.__dict__)
         for vert in CG.vertices:
             print(vert.__dict__)
